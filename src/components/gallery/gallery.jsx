ffa2ccca4f6e086df6a046f640df23b2501d2c3f
Cayetano <cayetano.rhernanz@gmail.com>
2023-02-10 14:57:29 +0100

    travail sur page accueil

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -2,7 +2,7 @@ import './gallery.scss'
 import Card from '../card/card'
 
 //importer les données du fichier de données
-import data from '../../data/data.json'
+import data from '../../data/data_logement.json'
 
 export default function gallery(){
     return (
