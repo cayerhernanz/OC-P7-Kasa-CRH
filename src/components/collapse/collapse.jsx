b53ed7650eadd2090931dc211f07ba3f5c6ac871
Cayetano <cayetano.rhernanz@gmail.com>
2023-02-14 15:22:07 +0100

    collapse page about ver 1

---
+++ src/components/collapse/collapse.jsx
@@ -0,0 +1,33 @@
+import './collapse.scss'
+import arrow from '../../assets/collapse-arrow.png'
+
+
+//importer useState pour définir l'etat 'ouverture du collapse
+import {useState} from 'react';
+
+//Variables de la fonction pour l'affichage dynamique à aprtir des données des JSON
+export default function Collapse({title, content}){
+
+    //Constante pour déterminer la page actuelle
+    const currentPage = window.location.pathname;
+
+    //Constantes toggle pour le hook pour l'ouverture du collapse
+    const [toggle, setToggle] = useState(false);
+
+    return (
+        <div className={currentPage === '/about' ? 'collapse_about' : 'collapse_logement'}>
+            <div className='collapse_header' onClick={() => setToggle(!toggle)}>
+                <h3>{title}</h3>
+                <img className={toggle ? 'arrow_up' : 'arrow_down'} src={arrow} alt="icone de flèche"/>
+            </div>
+            <div className={toggle ? 'collapse_content' : 'collpase_content_hidden'}>
+                {Array.isArray(content) ? content.map((item, index) => { //Récupérer le contenu du tableau de données
+                    return (
+                        <p key={index}>{item}</p>
+                    )
+                }) : content
+                }
+            </div>
+        </div>
+    )
+}
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,26 @@
 import './about.scss'
 
+//importer les données de la page à propos depuis le json
+import aboutData from '../../data/data_about.json'
+
 import Header from '../../components/header/header'
-import Banner from '../../components/banner2/banner2'
+import Banner2 from '../../components/banner2/banner2'
 import Footer from '../../components/footer/footer'
+import Collapse from '../../components/collapse/collapse'
 
 export default function About() {
     return (
         <body className='about'>
             <Header />
-            <Banner />
+            <Banner2 />
+            <main>
+                {aboutData.map(data => {
+                    return (
+                        <Collapse title={data.title} content={data.content}/>
+                    )
+                }
+                )}
+            </main>
             <Footer />
         </body>
     )
